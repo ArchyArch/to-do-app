06416ef6518cf4ce3dd723138494d124980eab26
wilku27 <szymonwilk420@gmail.com>
2019-04-15 22:44:16 +0200

    preparations

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Express server listening on port: ${port} ...`);
+})
